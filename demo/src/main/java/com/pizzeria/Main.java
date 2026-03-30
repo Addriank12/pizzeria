9e4998191aeb861d40261326f0f893492ec6a97b
Nicocc2003 <cordero.simon@es.uazuay.edu.ec>
2026-03-29 20:13:44 -0500

    hola

--- demo/src/main/java/com/pizzeria/Main.java
+++ demo/src/main/java/com/pizzeria/Main.java
@@ -1,16 +1,18 @@
 package com.pizzeria;
 
+import dataAcces.model.Cliente;
 import dataAcces.model.Empleado;
 import dataAcces.model.Empleado_Cargo;
+import dataAcces.repository.Cliente_Repository;
 import dataAcces.repository.Empleado_Repository;
 
 public class Main {
-    static Empleado_Repository empleado_repository = new Empleado_Repository();
+    static Cliente_Repository cliente_repository = new Cliente_Repository();
     
     
     public static void main(String[] args) {
-        Empleado empleado = new Empleado(0, "0106862832", "Nicolas Cordero", "Del rosedal 4-92", "09995112970", "nccmac2003@hotmail.com", Empleado_Cargo.mesero);
-        empleado_repository.Insert(empleado);
+        Cliente cliente = new Cliente(0, "0106862832", "Nicolas Cordero", "Del rosedal 4-92", "09995112970", "nccmac2003@hotmail.com");
+        cliente_repository.Insert(cliente);
         System.out.println("Hello world!");
     }
 }
